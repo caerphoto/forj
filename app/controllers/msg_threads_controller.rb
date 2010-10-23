72bf007fa66ce03ff3ea6eac7c183edb3c957ad7
caerphoto <github@caerphoto.com>
2010-10-23 18:54:20 +0100

    Deleted some javascript I left in msg_threads_controller.rb by mistake

--- app/controllers/msg_threads_controller.rb
+++ app/controllers/msg_threads_controller.rb
@@ -18,9 +18,3 @@ class MsgThreadsController < ApplicationController
         render :json => threads_array.to_json
     end
 end
-            "&reply_from=", FORJ.ui.selReplyFrom.val(),
-            "&thread=", post_data.thread,
-            "&reply_index=", post_data.post_index,
-            "&msg=", encodeURIComponent(FORJ.ui.replybox.find("textarea").
-                val()),
-            "&post_index=1" // for now at least
