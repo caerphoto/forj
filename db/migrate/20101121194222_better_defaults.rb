a1396e37cba166159879c896ba696682e7fda67d
caerphoto <github@caerphoto.com>
2010-11-22 00:15:25 +0000

    Man Postgres is fussy. Changed :limit for thread titles to 255, since apparently 100 isn't enough for one of the existing titles.

--- db/migrate/20101121194222_better_defaults.rb
+++ db/migrate/20101121194222_better_defaults.rb
@@ -2,7 +2,7 @@ class BetterDefaults < ActiveRecord::Migration
   def self.up
       change_column :folders, :name, :string, :null => false, :limit => 50
 
-      change_column :msg_threads, :title, :string, :null => false, :limit => 100
+      change_column :msg_threads, :title, :string, :null => false, :limit => 255
 
       # Defaults so they don't need to be specified when creating posts
       change_column :posts, :reply_user_id, :integer, :default => 0
